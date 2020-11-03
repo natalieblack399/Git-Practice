3f10ffa45aaa063bc8f02905ee79bad1d50b0e90
natalieblack399 <natalieblack399@users.noreply.github.com>
2020-11-03 10:22:08 -0700

    com

--- star-wars-characters/main.js
+++ star-wars-characters/main.js
@@ -19,23 +19,36 @@ otherButton.textContent = 'Other Characters'
 mainHeader.appendChild(otherButton)
 
 const maleCharacters = people.filter((person) => person.gender === 'male')
-console.log(maleCharacters)
 
-maleButton.addEventListener('click', (event) => {
-  maleCharacters.forEach((element) => {
-    const charFigure = document.createElement('figure')
-      const charImg = document.createElement('img')
-    let charNum = getLastNumber(element.url)
-    charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
-    const charCaption = document.createElement('figcaption')
-    charCaption.textContent = element.name
+const femaleCharacters = people.filter((person) => person.gender === 'female')
 
-    charFigure.appendChild(charImg)
-    charFigure.appendChild(charCaption)
-
-    mainContent.appendChild(charFigure)
-  })
+const otherCharacters = people.filter((thing) => {
+    if (thing.gender === 'n/a') {
+        return thing
+    }
 })
+
+maleButton.addEventListener('click', () => populateDOM(maleCharacters))
+
+femaleButton.addEventListener('click', () => populateDOM(femaleCharacters))
+
+function populateDOM(characters) {
+    removeChildren(mainContent)
+    characters.forEach((element) => {
+        const charFigure = document.createElement('figure')
+        const charImg = document.createElement('img')
+        let charNum = getLastNumber(element.url)
+        charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+        charImg.addEventListener('error', () => charImg.hidden = true) // genius level
+        const charCaption = document.createElement('figcaption')
+        charCaption.textContent = element.name
+    
+        charFigure.appendChild(charImg)
+        charFigure.appendChild(charCaption)
+    
+        mainContent.appendChild(charFigure)
+    })
+}
 
 // let theUrl = "https://swapi.co/api/people/2/"
 // let theUrl2 = "https://swapi.co/api/people/12/"
@@ -47,4 +60,10 @@ function getLastNumber(url) {
         start++
     }
     return url.slice(start, end)
-  }
+}
+
+function removeChildren(container) {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+      }
+}
