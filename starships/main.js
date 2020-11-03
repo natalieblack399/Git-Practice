c14961587e912dcc5d513c9ddfac3c44e495480c
natalieblack399 <natalieblack399@users.noreply.github.com>
2020-11-03 11:13:51 -0700

    v

--- starships/main.js
+++ starships/main.js
@@ -9,7 +9,9 @@ function populateNav(starships) {
         let anchorWrap = document.createElement('a')
         anchorWrap.href = '#'
         anchorWrap.addEventListener('click', event => {
-            console.log(event)
+            let shipName = event.target.textContent
+            const foundShip = starships.find(ship => ship.name === shipName)
+            populateShipView(foundShip)
         })
 
         let listItem = document.createElement('li')
@@ -19,6 +21,15 @@ function populateNav(starships) {
         navList.appendChild(anchorWrap)
         nav.appendChild(navList)
     })
+}
+
+function populateShipView(shipData) {
+    console.log(shipData)
+    // use createElement to make new img elements
+    // set their src to the url below
+    // make shipNum act like charNum from the characters page
+    //https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg
+    // append the image to the shipView element
 }
 
 populateNav(starships)
