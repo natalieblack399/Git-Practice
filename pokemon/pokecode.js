3a6689b029acb0941f145eb6d449b5ff3a353e0e
natalieblack399 <natalieblack399@users.noreply.github.com>
2020-11-17 10:55:27 -0700

    b

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -15,21 +15,34 @@ function loadPage() {
         (async (data) => {
             for (const pokemon of data.results) {
                 await getAPIData(pokemon.url).then((pokeData) => {
-                populatePokeCard(pokeData)
-            })
-        }
-    })
+                    console.log(pokeData)
+                    populatePokeCard(pokeData)
+                })
+            }
+        })
 }
 
 const pokemonGrid = document.querySelector('.pokemonGrid')
 
 function populatePokeCard(pokemon) {
+    let pokeScene = document.createElement('div')
+    pokeScene.className = 'scene'
+    let pokeCard = document.createElement('div')
+    pokeCard.className = 'card'
     let cardFront = document.createElement('div')
     let frontLabel = document.createElement('p')
+    let cardBack = document.createElement('div')
+    let backLabel = document.createElement('p')
+
 
     frontLabel.textContent = pokemon.name
+    backLabel.textContent = `I'm the back of the card`
     cardFront.appendChild(frontLabel)
-    pokemonGrid.appendChild(cardFront)
+    cardBack.appendChild(backLabel)
+    pokeCard.appendChild(cardFront)
+    pokeCard.appendChild(cardBack)
+    pokeScene.appendChild(pokeCard)
+    pokemonGrid.appendChild(pokeScene)
 }
 
 loadPage()
