764821827e074eeda4d03ffeb8f55bbe6cf73ed8
natalieblack399 <natalieblack399@users.noreply.github.com>
2020-11-17 11:19:56 -0700

    p

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -31,18 +31,29 @@ function populatePokeCard(pokemon) {
     pokeCard.className = 'card'
     let cardFront = document.createElement('div')
     let frontLabel = document.createElement('p')
+    let frontImage = document.createElement('img')
     let cardBack = document.createElement('div')
     let backLabel = document.createElement('p')
 
 
     frontLabel.textContent = pokemon.name
+    frontImage.src = `../images/pokemon/${getImageFileName(pokemon)}.png`
     backLabel.textContent = `I'm the back of the card`
+    cardFront.appendChild(frontImage)
     cardFront.appendChild(frontLabel)
     cardBack.appendChild(backLabel)
     pokeCard.appendChild(cardFront)
     pokeCard.appendChild(cardBack)
     pokeScene.appendChild(pokeCard)
     pokemonGrid.appendChild(pokeScene)
+}
+
+function getImageFileName(pokemon) {
+    if (pokemon.id < 10) {
+        return `00${pokemon.id}`
+    } else if (pokemon.id > 9 && pokemon.id < 99) {
+        return `0${pokemon.id}`
+    }
 }
 
 loadPage()
