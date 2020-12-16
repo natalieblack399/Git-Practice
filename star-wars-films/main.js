ee6ef03de8ad9d830986fbc2f04f63131e9ba6bc
natalieblack399 <natalieblack399@users.noreply.github.com>
2020-12-15 23:06:36 -0700

    let's be done

--- star-wars-films/main.js
+++ star-wars-films/main.js
@@ -19,14 +19,6 @@ for (let step = 0; step < 7; step++) {
 
     main.appendChild(figure)
   }
-
-/* for (const film of films) {
-    let newImg = document.createElement('img') // new image instance
-    newImg.src = 'https://starwars-visualguide.com/assets/img/films/2.jpg' // set the source of it or nothing will show
-    // now append the image to the DOM somehow
-    main.appendChild(newImg)
-    console.log(film.title)
-  } */
   
 
 
