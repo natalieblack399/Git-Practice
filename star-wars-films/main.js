2fa33ca78c565ee0e9c9e13cd358185b0d02322e
natalieblack399 <natalieblack399@users.noreply.github.com>
2020-12-15 22:38:42 -0700

    m

--- star-wars-films/main.js
+++ star-wars-films/main.js
@@ -1,5 +1,4 @@
 import { films } from '../data/films.js'
-import { getLastNumber } from '../utils/index.js'
 
 const main = document.querySelector('main')
 
@@ -10,7 +9,7 @@ for (let step = 0; step < 7; step++) {
     let figImg = document.createElement('img')
     figImg.src = `https://starwars-visualguide.com/assets/img/films/${step + 1}.jpg` 
     let figCaption = document.createElement('figcaption')
-    
+
     const foundFilm = films.find(film => parseInt(getLastNumber(film.url)) === (step + 1))
     figCaption.textContent = foundFilm.title
 
@@ -19,3 +18,15 @@ for (let step = 0; step < 7; step++) {
 
     main.appendChild(figure)
   }
+
+/* for (const film of films) {
+    let newImg = document.createElement('img') // new image instance
+    newImg.src = 'https://starwars-visualguide.com/assets/img/films/2.jpg' // set the source of it or nothing will show
+    // now append the image to the DOM somehow
+    main.appendChild(newImg)
+    console.log(film.title)
+  } */
+  
+
+
+//https://starwars-visualguide.com/assets/img/characters/2.jpg
